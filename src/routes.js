f6d45e7815d41a60a2091d0698d234585fd779fe
Denner Rondinely <denner.rondinely@gmail.com>
2020-01-29 20:01:08 -0200

    Criando rota de autenticaçao do usuario

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserCpntroller';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
