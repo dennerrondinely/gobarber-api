4c036c1ab66a045898742cd0c1cc89be44e47f40
Denner Rondinely <denner.rondinely@gmail.com>
2020-01-29 20:43:48 -0200

    Criar ediçao de usuario

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserCpntroller';
 import SessionController from './app/controllers/SessionController';
 
+import auth from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
-
 routes.post('/sessions', SessionController.store);
+routes.post('/users', UserController.store);
+
+routes.use(auth);
+
+routes.put('/users', UserController.update);
 
 export default routes;
