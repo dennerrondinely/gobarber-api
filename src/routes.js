644d407b3c152c53c18f1a98b8c7b66333c9960e
Denner Rondinely <denner.rondinely@gmail.com>
2020-02-01 19:34:32 -0200

    Criar servoço para salvar avatar do usuario

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserCpntroller';
+import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import auth from './app/middlewares/auth';
 
@@ -16,8 +17,6 @@ routes.post('/users', UserController.store);
 routes.use(auth);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
