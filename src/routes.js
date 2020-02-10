3ae108cd29803735a5b980a580dec7dc44e3e45e
Denner Rondinely <denner.rondinely@gmail.com>
2020-02-09 22:36:34 -0200

    Criar lista de horarios disponiveis

--- src/routes.js
+++ src/routes.js
@@ -9,8 +9,9 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
-import auth from './app/middlewares/auth';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,11 +19,12 @@ const upload = multer(multerConfig);
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
 
-routes.use(auth);
+routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
